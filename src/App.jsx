4614383a1bce58993cc6c6955647b312fc3d9626
david-lafontant <lafontant.david@gmail.com>
2022-11-15 07:40:15 -0600

    Display weather data in app

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,40 @@ function App() {
   const [weather, setWeather] = useState({})
 
   const search = async (e) => {
-    if(e.key === 'Enter'){
+    if (e.key === 'Enter') {
       const data = await fetchWeather(query);
       setWeather(data);
       console.log(data);
       setQuery('');
     }
-
-
   }
 
   return (
     <div className="App">
-    <input type="text" placeholder="Search..." className= 'search' value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search}></input>
+      <div classname="input">
+        <input type="text" className="search" placeholder="Search..." className='search' value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search}></input>
+
+      </div>
+
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span className="black">{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <p className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+
+          </p>
+          <p className="icon-weather">
+            <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+          </p>
+          <p className="black" id="description">
+            {weather.weather[0].description}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
