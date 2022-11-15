304100380c8304e0a54bf66ae341fa993a18e133
david-lafontant <lafontant.david@gmail.com>
2022-11-15 08:14:51 -0600

    Use "export default"

--- src/api/fetchApi.js
+++ src/api/fetchApi.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
-const API_KEY = "f4eb1085b70f3edd0b49c42a550cf22b";
-const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = 'f4eb1085b70f3edd0b49c42a550cf22b';
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
-export const fetchWeather = async (query) => {
+const fetchWeather = async (query) => {
   const { data } = await axios.get(API_URL, {
     params: {
       q: query,
-      units: "metric",
+      units: 'metric',
       APPID: API_KEY,
-    }
+    },
   });
   return data;
-}
+};
+
+export default fetchWeather;
