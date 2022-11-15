7703b36f1602efb56f2584c05e560fc0ab9d4fc7
david-lafontant <lafontant.david@gmail.com>
2022-11-15 07:04:52 -0600

    Retrieve data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
+
 import './App.css';
+import { fetchWeather } from './api/fetchApi';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({})
+
+  const search = async (e) => {
+    if(e.key === 'Enter'){
+      const data = await fetchWeather(query);
+      setWeather(data);
+      console.log(data);
+      setQuery('');
+    }
+
+
+  }
 
   return (
     <div className="App">
-      <h1>Weather App</h1>
+    <input type="text" placeholder="Search..." className= 'search' value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search}></input>
     </div>
   );
 }
